343c93c293c124ed8bdfc763082becc7e03a16fa
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions are being created that cache the inverse of
+## a matrix and such that next time the user calculates the matrix
+## inverse, the previously calculated and saved value is returned
+## instead of calculating the value again.
 
-## Write a short comment describing this function
+## The first function, makeCacheMatrix, creates a special "matrix" object, which 
+## is really a list containing the function that performs the following
+## actions:
+## 1.- sets the value of the matrix
+## 2.- gets the value of the matrix
+## 3.- sets the value of the inverse
+## 4.- gets the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## create a matrix object x and some associated sub-functions/methods
+        ## define the cache m
+        m <- NULL
+        set <- function(y) {
+                x <<- y ## assign the input matrix y to the variable x in the
+                ## parent environment
+                m <<- NULL ## re-initialize m in the parent environment to null
+        }
+        get <- function() x ## return the matrix x
+        setinverse <- function(inverse) m <<- inverse ## set the cache m equal
+        ## to the inverse of the matrix x
+        getinverse <- function() m ## return the cached inverse of x
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The next function, cacheSolve, calculates the inverse of the special matrix
+## created with the previous function.  It first checks if the inverse has been
+## already calculated.  If this is the case, it obtains the inverse from the
+## cache and skips the calculation.  If not, it calculates the matrix inverse
+## and sets the value of the inverse of the inverse in the cache via the
+## "setinverse" function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
