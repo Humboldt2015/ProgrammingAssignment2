1af334fe69ef6406e38979a3cd8862a914c903c0
Francisco Olivas <olivasfa@outlook.com>
2015-03-20 17:41:00 -0600

    Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -4,27 +4,29 @@
 ## instead of calculating the value again.
 
 ## The first function, makeCacheMatrix, creates a special "matrix" object, which 
-## is really a list containing the function that performs the following
-## actions:
+## a list containing the function that performs the following:
 ## 1.- sets the value of the matrix
 ## 2.- gets the value of the matrix
 ## 3.- sets the value of the inverse
 ## 4.- gets the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+        ## creates a matrix object x and some associated sub-functions
+        m <- NULL ## defines the cache m
         set <- function(y) {
-                x <<- y
-                m <<- NULL
+                x <<- y ## assigns the imput matrix y to the variable x in
+                        ## the parent environment
+                m <<- NULL ## re-initializes m in the parent environment to null
         }
-        get <- function() x
-        setinverse <- function(inverse) m <<- inverse
-        getinverse <- function() m
+        get <- function() x ## returns the matrix x
+        setinverse <- function(inverse) m <<- inverse ## sets the cache m equal
+                                                      ## to the inverse of the
+                                                      ## matrix x
+        getinverse <- function() m ## returns the cached inverse of x
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
 }
-
 
 ## The next function, cacheSolve, calculates the inverse of the special matrix
 ## created with the previous function.  It first checks if the inverse has been
@@ -34,6 +36,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## "setinverse" function.
 
 cacheSolve <- function(x, ...) {
+        ## returns a matrix that is the inverse of "x"
         m <- x$getinverse()
         if(!is.null(m)) {
                 message("getting cached data")
